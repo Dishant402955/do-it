a65e5a654eb7dcd99fc0ce609263e2e7547a8b9b
Dishant radadiya <dishantradadiya3036@gmail.com>
2025-06-14 11:16:57 +0530

    Setup Clerk with custom pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import {
+	ClerkProvider,
+	SignedIn,
+	SignedOut,
+	SignInButton,
+	SignUpButton,
+	UserButton,
+} from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
 
 export const metadata: Metadata = {
 	title: "Do it",
@@ -12,8 +23,39 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={`antialiased`}>{children}</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en" suppressHydrationWarning>
+				<body className={`antialiased`}>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
+						<header className="fixed top-0 w-full flex justify-between items-center p-4 gap-4 h-16 border-b shadow-accent-foreground/10 shadow-lg dark:shadow-accent">
+							<div>
+								<ModeToggle />
+							</div>
+							<div>
+								<SignedOut>
+									<div className="space-x-8">
+										<SignInButton>
+											<Button variant={"ghost"}>Login</Button>
+										</SignInButton>
+										<SignUpButton>
+											<Button>Do It for Free!</Button>
+										</SignUpButton>
+									</div>
+								</SignedOut>
+								<SignedIn>
+									<UserButton />
+								</SignedIn>
+							</div>
+						</header>
+						{children}
+					</ThemeProvider>
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
