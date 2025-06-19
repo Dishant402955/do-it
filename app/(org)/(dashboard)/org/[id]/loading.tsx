3534e50971afaa67f47aa5043abccd1241d2bb04
Dishant radadiya <dishantradadiya3036@gmail.com>
2025-06-19 22:02:24 +0530

    current org & tab indicator

--- app/(org)/(dashboard)/org/[id]/loading.tsx
+++ app/(org)/(dashboard)/org/[id]/loading.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Loading = () => {
 	return (
-		<Skeleton className=" w-full h-full flex flex-col p-6">
+		<div className=" w-full h-full flex flex-col p-6">
 			<hr className="mt-8" />
 
 			<div className="flex gap-4 mt-6">
@@ -13,29 +13,29 @@ const Loading = () => {
 			</div>
 
 			<div className="grid grid-cols-5 space-y-4 my-8 w-[80%] ml-20">
-				<div className="h-36 w-44 rounded-lg bg-accent/50 p-0">
+				<Skeleton className="h-36 w-44 rounded-lg bg-accent/50 p-0">
 					<div className="size-full p-2 flex justify-center items-center">
 						<p className="font-bold text-2xl bg-accent-foreground dark:bg-accent h-16 w-36 rounded-lg flex justify-center items-center">
 							{}
 						</p>
 					</div>
-				</div>
-				<div className="h-36 w-44 rounded-lg bg-accent/50 p-0">
+				</Skeleton>
+				<Skeleton className="h-36 w-44 rounded-lg bg-accent/50 p-0">
 					<div className="size-full p-2 flex justify-center items-center">
 						<p className="font-bold text-2xl bg-accent-foreground dark:bg-accent h-16 w-36 rounded-lg flex justify-center items-center">
 							{}
 						</p>
 					</div>
-				</div>
-				<div className="h-36 w-44 rounded-lg bg-accent/50 p-0">
+				</Skeleton>
+				<Skeleton className="h-36 w-44 rounded-lg bg-accent/50 p-0">
 					<div className="size-full p-2 flex justify-center items-center">
 						<p className="font-bold text-2xl bg-accent-foreground dark:bg-accent h-16 w-36 rounded-lg flex justify-center items-center">
 							{}
 						</p>
 					</div>
-				</div>
+				</Skeleton>
 			</div>
-		</Skeleton>
+		</div>
 	);
 };
 
--- components/orgs.tsx
+++ components/orgs.tsx
@@ -12,12 +12,14 @@ import {
 } from "lucide-react";
 import { SidebarMenuItem, useSidebar } from "./ui/sidebar";
 import Link from "next/link";
-import { useOrganizationList } from "@clerk/nextjs";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 import Image from "next/image";
 import { Suspense } from "react";
 import { Skeleton } from "./ui/skeleton";
 import OrgsSkeleton from "./orgs-skeleton";
 import { unstable_ViewTransition as ViewTransition } from "react";
+import { cn } from "@/lib/utils";
+import { redirect, usePathname } from "next/navigation";
 
 const Orgs = () => {
 	const { userMemberships, isLoaded, setActive } = useOrganizationList({
@@ -25,32 +27,43 @@ const Orgs = () => {
 			infinite: true,
 		},
 	});
+	const { organization } = useOrganization();
 	const { open } = useSidebar();
+	const pathname = usePathname();
+	const current =
+		pathname.split("/").length === 3 ? "boards" : pathname.split("/")[3];
 
 	if (!isLoaded) {
 		return <OrgsSkeleton />;
 	}
 
+	if (!organization) {
+		return redirect("/select-org");
+	}
 	const items = [
 		{
 			title: "Boards",
 			url: ``,
 			icon: CircuitBoardIcon,
+			slug: "boards",
 		},
 		{
 			title: "Activity",
 			url: `activity`,
 			icon: ActivityIcon,
+			slug: "activity",
 		},
 		{
 			title: "Settings",
 			url: `settings`,
 			icon: Settings,
+			slug: "settings",
 		},
 		{
 			title: "Billings",
 			url: `billings`,
 			icon: CreditCard,
+			slug: "billings",
 		},
 	];
 
@@ -71,7 +84,17 @@ const Orgs = () => {
 						) => (
 							<SidebarMenuItem key={index}>
 								<AccordionItem value={`${index}`}>
-									<AccordionTrigger className="flex w-full items-center">
+									<AccordionTrigger
+										className={cn(
+											"flex w-full items-center my-1",
+											org.organization.id === organization.id
+												? open
+													? "bg-neutral-950 border-[1px] border-white"
+													: ""
+												: "",
+											open ? "p-3" : ""
+										)}
+									>
 										<Suspense
 											fallback={
 												<Skeleton>
@@ -85,8 +108,14 @@ const Orgs = () => {
 											<Image
 												src={org.organization.imageUrl}
 												alt="org"
-												height={34}
-												width={34}
+												height={40}
+												width={40}
+												className={cn(
+													"flex size-[40px]",
+													organization.id === org.organization.id &&
+														!open &&
+														" border-2 border-white p-[2px]"
+												)}
 											/>
 											<Title title={org.organization.name} />
 										</Suspense>
@@ -96,21 +125,27 @@ const Orgs = () => {
 											{items.map((item, index) => (
 												<li
 													key={index}
-													className={`w-full flex justify-start items-center gap-4 hover:bg-accent`}
+													className={cn(
+														`w-full flex justify-start items-center gap-4 p-2`
+													)}
 												>
 													<Link
 														href={`/org/${org.organization.id}/${item.url}`}
 														onClick={() => {
 															setActive({ organization: org.organization.id });
 														}}
-														className={`flex gap-4 size-full  ${
+														className={cn(
+															`flex gap-4 size-full hover:bg-accent rounded-lg`,
 															open
 																? "p-2 pl-3"
-																: "p-1 py-2 justify-center items-center"
-														}`}
+																: "p-1 py-2 justify-center items-center",
+															org.organization.id === pathname.split("/")[2] &&
+																current === item.slug &&
+																"bg-neutral-300 text-black hover:bg-accent-foreground"
+														)}
 													>
 														<item.icon
-															className={`flex justify-center items-center h-5 w-5`}
+															className={`flex justify-center items-center h-6 w-6`}
 														/>
 														<p className={open ? "" : "hidden"}>{item.title}</p>
 													</Link>
